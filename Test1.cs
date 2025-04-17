e4461173ece070354e40ee1894484089e81579de
BobinVladislavv <vlad.bobin17@gmail.com>
2025-04-17 10:59:23 +0300

    Создание файлов

---
+++ Test1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using laba2;
+namespace SortingTests
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void SortIfOneItem()
+        {
+
+        }
+    }
+}
